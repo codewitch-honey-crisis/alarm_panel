951f327cc68a0a09d4f22fadb37bd4f47c509231
codewitch-honey-crisis <gaztoast@gmail.com>
2025-07-21 21:43:39 -0700

    changed build tools to python

--- vite_clasp.py
+++ vite_clasp.py
@@ -14,4 +14,5 @@ else:
 
 print("ClASP Suite integration enabled")
 
-env.Execute("dotnet ./build_tools/clasptree.dll ./react-web/dist ./common/include/httpd_content.h --prefix httpd_ --epilogue ./common/include/httpd_epilogue.h --state context --block httpd_send_block --expr httpd_send_expr --handlers extended")
+#env.Execute("dotnet ./build_tools/clasptree.dll ./react-web/dist ./common/include/httpd_content.h --prefix httpd_ --epilogue ./common/include/httpd_epilogue.h --state context --block httpd_send_block --expr httpd_send_expr --handlers extended")
+env.Execute("python clasptree.py ./react-web/dist -o ./common/include/httpd_content.h -p httpd_ -E ./common/include/httpd_epilogue.h -s context -b httpd_send_block -e httpd_send_expr -H extended")
